47759b832d04fbbb03fd31acf202b771d861e582
Jan Pineda <jan.pineda@mnltechnology.com>
2026-06-27 09:39:19 +0800

    Update about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>This Pokémon Card website is a comprehensive platform designed for collectors, 
-      players, and enthusiasts to explore, trade, and track Pokémon trading cards. 
-      The website offers detailed information about individual cards to manage collections.</div>
+    // <div>This Pokémon Card website is a comprehensive platform designed for collectors, 
+    //   players, and enthusiasts to explore, trade, and track Pokémon trading cards. 
+    //   The website offers detailed information about individual cards to manage collections.</div>
+      <div>Pokemon Card Website.</div>
   )
 }
 
